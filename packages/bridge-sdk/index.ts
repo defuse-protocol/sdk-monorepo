51c9025b31440768fa9e3d050287076e54498d9e
Old Emil <cawabunga@defuse.org>
2025-06-19 09:38:06 +0200

    chore(bridge-sdk): export FeeExceedsAmountError

--- packages/bridge-sdk/index.ts
+++ packages/bridge-sdk/index.ts
@@ -1,3 +1,4 @@
 export * from "./src/sdk";
 export * from "./src/intents/intent-signer-impl";
 export type { FeeEstimation } from "./src/shared-types";
+export { FeeExceedsAmountError } from "./src/classes/errors";
