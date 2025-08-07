433418f7acd9a55b51fa0f6383963da4132bdff4
Old Emil <cawabunga@defuse.org>
2025-08-07 09:04:34 +0200

    refactor(internal-utils): strong-type ILogger

--- packages/internal-utils/src/logger.ts
+++ packages/internal-utils/src/logger.ts
@@ -1,6 +1,10 @@
-export interface ILogger {
-	error(...args: unknown[]): void;
-	warn(...args: unknown[]): void;
-	info(...args: unknown[]): void;
-	debug(...args: unknown[]): void;
+type Context = Record<string, unknown>;
+
+export interface ILogger
+	extends Pick<typeof console, "error" | "warn" | "info" | "debug" | "trace"> {
+	error(msg: string | Error, ctx?: Context): void;
+	warn(msg: string, ctx?: Context): void;
+	info(msg: string, ctx?: Context): void;
+	debug(msg: string, ctx?: Context): void;
+	trace(msg: string, ctx?: Context): void;
 }
