390c9389af7264757920d939f5def892abfce01c
Old Emil <cawabunga@defuse.org>
2025-09-30 09:42:32 +0200

    test(intents-sdk): move `validateOmniToken()` tests to omni-bridge.test.ts

--- packages/intents-sdk/src/bridges/omni-bridge/omni-bridge.test.ts
+++ packages/intents-sdk/src/bridges/omni-bridge/omni-bridge.test.ts
@@ -1,37 +1,11 @@
 import { describe, expect, it } from "vitest";
 import { OmniBridge } from "./omni-bridge";
-import { validateOmniToken } from "./omni-bridge-utils";
 import { createOmniBridgeRoute } from "../../lib/route-config-factory";
 import { Chains } from "../../lib/caip2";
 import { UnsupportedAssetIdError } from "../../classes/errors";
 import { TokenNotFoundInDestinationChainError } from "./error";
 
 describe("OmniBridge", () => {
-	describe("validateOmniToken() works for valid token formats", () => {
-		it.each([
-			"eth.bridge.near",
-			"sol.omdep.near",
-			"base.omdep.near",
-			"arb.omdep.near",
-			"foo.omdep.near",
-			"aaaaaa20d9e0e2461697782ef11675f668207961.factory.bridge.near",
-			"sol-ABC123.omdep.near",
-			"arb-ABC123.omdep.near",
-			"base-ABC123.omdep.near",
-		])("validate", async (assetId) => {
-			expect(validateOmniToken(assetId)).toBe(true);
-		});
-		it.each([
-			"eth.Hellobridge.near",
-			"sol-ABC123.test.hello.near",
-			"aaaaaa20d9e0e2461697782ef11675f668207961.factory.bridgeomni.near",
-			"btc.omft.near",
-			"v3_1.omni.hot.tg:56_11111111111111111111",
-			"17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1", // USDC
-		])("validate", async (assetId) => {
-			expect(validateOmniToken(assetId)).toBe(false);
-		});
-	});
 	describe.sequential("supports()", () => {
 		it.each([
 			"nep141:eth.bridge.near",
