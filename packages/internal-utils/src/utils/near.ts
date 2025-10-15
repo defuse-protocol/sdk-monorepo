423cf4f965a36dbe459d3bc2348fad2780d31e11
jobotics <jo_botics@proton.me>
2025-10-15 10:17:12 +0100

    chore: apply lint fixes for code consistency

--- packages/internal-utils/src/utils/near.ts
+++ packages/internal-utils/src/utils/near.ts
@@ -76,52 +76,53 @@ export async function queryContract<
 }
 
 // Copied from https://github.com/near/near-account-id-rs/blob/8174b47afdc608feefc0949d23c9c14bb810b544/src/validation.rs#L58C42-L58C97
-const ACCOUNT_ID_REGEX = /^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/
-const MIN_ACCOUNT_ID_LENGTH = 2
-const ETH_IMPLICIT_ACCOUNT_LENGTH = 42
-const NEAR_IMPLICIT_ACCOUNT_LENGTH = 64
+const ACCOUNT_ID_REGEX =
+	/^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/;
+const MIN_ACCOUNT_ID_LENGTH = 2;
+const ETH_IMPLICIT_ACCOUNT_LENGTH = 42;
+const NEAR_IMPLICIT_ACCOUNT_LENGTH = 64;
 
 export function validateNearAddress(accountId: string): boolean {
-  if (
-    accountId.length < MIN_ACCOUNT_ID_LENGTH ||
-    accountId.length > NEAR_IMPLICIT_ACCOUNT_LENGTH
-  ) {
-    return false
-  }
-  if (isImplicitAccount(accountId)) {
-    return true
-  }
-  return ACCOUNT_ID_REGEX.test(accountId)
+	if (
+		accountId.length < MIN_ACCOUNT_ID_LENGTH ||
+		accountId.length > NEAR_IMPLICIT_ACCOUNT_LENGTH
+	) {
+		return false;
+	}
+	if (isImplicitAccount(accountId)) {
+		return true;
+	}
+	return ACCOUNT_ID_REGEX.test(accountId);
 }
 
 function isEthImplicitAccount(accountId: string): boolean {
-  return (
-    accountId.length === ETH_IMPLICIT_ACCOUNT_LENGTH &&
-    accountId.startsWith("0x") &&
-    /^[0-9a-f]+$/i.test(accountId.slice(2))
-  )
+	return (
+		accountId.length === ETH_IMPLICIT_ACCOUNT_LENGTH &&
+		accountId.startsWith("0x") &&
+		/^[0-9a-f]+$/i.test(accountId.slice(2))
+	);
 }
 
 function isNearDeterministic(accountId: string): boolean {
-  return (
-    accountId.length === ETH_IMPLICIT_ACCOUNT_LENGTH &&
-    accountId.startsWith("0s") &&
-    /^[0-9a-f]+$/i.test(accountId.slice(2))
-  )
+	return (
+		accountId.length === ETH_IMPLICIT_ACCOUNT_LENGTH &&
+		accountId.startsWith("0s") &&
+		/^[0-9a-f]+$/i.test(accountId.slice(2))
+	);
 }
 
 function isNearImplicit(accountId: string): boolean {
-  return (
-    accountId.length === NEAR_IMPLICIT_ACCOUNT_LENGTH &&
-    /^[0-9a-f]+$/i.test(accountId)
-  )
+	return (
+		accountId.length === NEAR_IMPLICIT_ACCOUNT_LENGTH &&
+		/^[0-9a-f]+$/i.test(accountId)
+	);
 }
 
 export function isImplicitAccount(accountId: string): boolean {
-  return (
-    (isEthImplicitAccount(accountId) ||
-      isNearImplicit(accountId) ||
-      isNearDeterministic(accountId)) &&
-    !accountId.includes(".")
-  )
+	return (
+		(isEthImplicitAccount(accountId) ||
+			isNearImplicit(accountId) ||
+			isNearDeterministic(accountId)) &&
+		!accountId.includes(".")
+	);
 }
