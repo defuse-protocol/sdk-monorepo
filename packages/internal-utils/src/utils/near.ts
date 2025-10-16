160a024e833889221bb3b6ad49f370d01fb01c52
jobotics <jo_botics@proton.me>
2025-10-16 10:28:18 +0100

    feat(internal-utils): enhanced Near account validation (#160)
    * feat(internal-utils): enhanced Near account validation
    
    * chore: apply lint fixes for code consistency
    
    * chore: set version changes to patch
    
    * fix: remove unnecessary check for dot existence

--- packages/internal-utils/src/utils/near.ts
+++ packages/internal-utils/src/utils/near.ts
@@ -5,7 +5,6 @@ import type {
 	Finality,
 } from "near-api-js/lib/providers/provider";
 import * as v from "valibot";
-import { isAddress } from "viem";
 
 /**
  * Use this function to decode a raw response from `nearClient.query()`
@@ -76,29 +75,53 @@ export async function queryContract<
 	return decodeQueryResult(response, schema);
 }
 
-// Copied from https://github.com/mynearwallet/my-near-wallet/blob/3b1a6c6e5c62a0235f5e32d370f803fa2180c6f8/packages/frontend/src/utils/wallet.ts#L75
-
+// Copied from https://github.com/near/near-account-id-rs/blob/8174b47afdc608feefc0949d23c9c14bb810b544/src/validation.rs#L58C42-L58C97
 const ACCOUNT_ID_REGEX =
 	/^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/;
+const MIN_ACCOUNT_ID_LENGTH = 2;
+const ETH_IMPLICIT_ACCOUNT_LENGTH = 42;
+const NEAR_IMPLICIT_ACCOUNT_LENGTH = 64;
 
-const IMPLICIT_ACCOUNT_MAX_LENGTH = 64;
-
-export function isLegitAccountId(accountId: string): boolean {
-	// NEAR account must be at least 2 characters long
-	if (accountId.length < 2) {
+export function validateNearAddress(accountId: string): boolean {
+	if (
+		accountId.length < MIN_ACCOUNT_ID_LENGTH ||
+		accountId.length > NEAR_IMPLICIT_ACCOUNT_LENGTH
+	) {
 		return false;
 	}
-	// EVM-like account check
-	if (isAddress(accountId) && accountId === accountId.toLowerCase()) {
+	if (isImplicitAccount(accountId)) {
 		return true;
 	}
-
-	// Explicit and implicit account check
 	return ACCOUNT_ID_REGEX.test(accountId);
+}
+
+function isEthImplicitAccount(accountId: string): boolean {
+	return (
+		accountId.length === ETH_IMPLICIT_ACCOUNT_LENGTH &&
+		accountId.startsWith("0x") &&
+		/^[0-9a-f]+$/i.test(accountId.slice(2))
+	);
+}
+
+function isNearDeterministic(accountId: string): boolean {
+	return (
+		accountId.length === ETH_IMPLICIT_ACCOUNT_LENGTH &&
+		accountId.startsWith("0s") &&
+		/^[0-9a-f]+$/i.test(accountId.slice(2))
+	);
+}
+
+function isNearImplicit(accountId: string): boolean {
+	return (
+		accountId.length === NEAR_IMPLICIT_ACCOUNT_LENGTH &&
+		/^[0-9a-f]+$/i.test(accountId)
+	);
 }
 
 export function isImplicitAccount(accountId: string): boolean {
 	return (
-		accountId.length === IMPLICIT_ACCOUNT_MAX_LENGTH && !accountId.includes(".")
+		isEthImplicitAccount(accountId) ||
+		isNearImplicit(accountId) ||
+		isNearDeterministic(accountId)
 	);
 }
