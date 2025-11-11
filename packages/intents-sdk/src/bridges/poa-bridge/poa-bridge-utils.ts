af7768d702933cc67c099481b5fe14fab174e02c
depressedPlumber502 <depressed.plumber.502@defuse.org>
2025-11-11 15:02:40 +0400

    fix: poa litecoin withdrawal

--- packages/intents-sdk/src/bridges/poa-bridge/poa-bridge-utils.ts
+++ packages/intents-sdk/src/bridges/poa-bridge/poa-bridge-utils.ts
@@ -54,6 +54,7 @@ const caip2Mapping = {
 	[Chains.Sui]: "sui:mainnet",
 	[Chains.Aptos]: "aptos:mainnet",
 	[Chains.Cardano]: "cardano:mainnet",
+	[Chains.Litecoin]: "ltc:mainnet",
 } satisfies Record<
 	string,
 	(typeof poaBridge.PoaBridgeNetworkReference)[Exclude<
@@ -84,6 +85,7 @@ const tokenPrefixMapping = {
 	sui: Chains.Sui,
 	aptos: Chains.Aptos,
 	cardano: Chains.Cardano,
+	ltc: Chains.Litecoin,
 };
 
 export function contractIdToCaip2(contractId: string): Chain {
