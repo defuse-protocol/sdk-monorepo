2a0f28d1238e5b29be69a2d8a5f88fa452d9e2c2
depressedPlumber502 <depressed.plumber.502@defuse.org>
2025-11-26 23:03:59 +0400

    refactor: remove btc connector

--- packages/intents-sdk/src/bridges/omni-bridge/omni-bridge-constants.ts
+++ packages/intents-sdk/src/bridges/omni-bridge/omni-bridge-constants.ts
@@ -2,4 +2,3 @@ export const NEAR_NATIVE_ASSET_ID = "nep141:wrap.near";
 export const OMNI_BRIDGE_CONTRACT = "omni.bridge.near";
 export const MIN_ALLOWED_STORAGE_BALANCE_FOR_INTENTS_NEAR =
 	500000000000000000000000n; // 0.5 NEAR
-export const BTC_BRIDGE_CONNECTOR = "btc-connector.bridge.near";
