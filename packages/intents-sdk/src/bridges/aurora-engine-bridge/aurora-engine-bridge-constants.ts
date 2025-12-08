2f78c2038a1d0e5bc914af6e33189290dd7b08fb
Old Emil <cawabunga@defuse.org>
2025-12-08 20:01:22 +0100

    feat(intents-sdk): add min_gas to aurora-engine-bridge withdrawals

--- packages/intents-sdk/src/bridges/aurora-engine-bridge/aurora-engine-bridge-constants.ts
+++ packages/intents-sdk/src/bridges/aurora-engine-bridge/aurora-engine-bridge-constants.ts
@@ -1,10 +1,7 @@
 export const NEAR_NATIVE_ASSET_ID = "nep141:wrap.near";
 
-/**
- * This was chosen empirically and may not be optimal, it includes 10% margin.
- *
- * NOT USED: Other virtual chains with non-ETH base tokens may require more gas.
- *
- * TODO: Estimate gas for other virtual chains and set in `ft_withdraw`.
- */
+// This was chosen empirically and may not be optimal, it includes 10% margin.
 export const MIN_GAS_AMOUNT = "38500000000000"; // 38.5 tgas
+
+// For virtual chains with non-18 decimal base tokens. Not tested, chosen arbitrarily.
+export const MIN_GAS_AMOUNT_NON_STANDARD_DECIMALS = "100000000000000"; // 100 tgas
