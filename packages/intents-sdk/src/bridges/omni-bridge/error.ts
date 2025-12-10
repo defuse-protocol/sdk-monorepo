9d795a580e9e37cb129e563d2edb61379dcf6997
Old Emil <cawabunga@defuse.org>
2025-12-11 00:23:53 +0100

    refactor(intents-sdk): remove unused withdrawal error classes

--- packages/intents-sdk/src/bridges/omni-bridge/error.ts
+++ packages/intents-sdk/src/bridges/omni-bridge/error.ts
@@ -1,37 +1,6 @@
 import { BaseError } from "@defuse-protocol/internal-utils";
 import type { Chain } from "../../lib/caip2";
 
-export type OmniTransferNotFoundErrorType = OmniTransferNotFoundError & {
-	name: "OmniTransferNotFoundError";
-};
-export class OmniTransferNotFoundError extends BaseError {
-	constructor(public txHash: string) {
-		super("Omni transfer with given hash is not found in the relayer.", {
-			metaMessages: [`OriginTxHash: ${txHash}`],
-			name: "OmniTransferNotFoundError",
-		});
-	}
-}
-
-export type OmniTransferDestinationChainHashNotFoundErrorType =
-	OmniTransferDestinationChainHashNotFoundError & {
-		name: "OmniTransferDestinationChainHashNotFoundError";
-	};
-export class OmniTransferDestinationChainHashNotFoundError extends BaseError {
-	constructor(
-		public txHash: string,
-		public destinationChain: string | undefined,
-	) {
-		super("Relayer did not return destination chain hash for a transfer.", {
-			metaMessages: [
-				`OriginTxHash: ${txHash}`,
-				`DestinationChain: ${destinationChain}`,
-			],
-			name: "OmniTransferDestinationChainHashNotFoundError",
-		});
-	}
-}
-
 export type TokenNotFoundInDestinationChainErrorType =
 	TokenNotFoundInDestinationChainError & {
 		name: "TokenNotFoundInDestinationChainError";
